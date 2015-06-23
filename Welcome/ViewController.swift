9e3f0a46216f9881be838615701e9be70da96bd8
Matt-Brooks <mkabrooks@gmail.com>
2015-06-23 20:52:57 +0100

    Initial Commit

---
+++ Welcome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Welcome
+//
+//  Created by Matt Brooks on 23/06/2015.
+//  Copyright (c) 2015 Matt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
